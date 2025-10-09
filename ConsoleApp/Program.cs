fdca3f301d90d3269059a79fc83caafc417ccb79
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-10-09 20:31:29 -0300

    Interação usuario e maquina (basico)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,7 +5,14 @@ public class Program
 { 
     public static void Main() 
     { 
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("-----Cadastro de usuario-----");
+        Console.Write("Digite o seu nome: ");
+        string name = Console.ReadLine();
+
+
+        Console.WriteLine("Digite sua idade: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Seu nome é " + name + "Sua idade é: " + age);
     }
 
 }
