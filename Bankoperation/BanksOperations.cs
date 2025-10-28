efeaddac325d7341228ee7f24a31bd86245785bb
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-10-28 19:07:58 -0300

    Criação de WithDraw

--- Bankoperation/BanksOperations.cs
+++ Bankoperation/BanksOperations.cs
@@ -21,5 +21,26 @@
                 Console.WriteLine("Valor Inválido. Tente novamente");
             };
         }
+
+        public void WithDraw()
+        {
+            Console.WriteLine("Informe o valor a ser retirado: R$ ");
+            if (decimal.TryParse(Console.ReadLine(), out decimal amount) && amount > 0)
+            {
+                if(amount <= balance)
+                {
+                    Console.WriteLine($"Valor de R$ {amount} retirado com sucesso!");
+                }
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente para essa operação.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor Inválido. Tente novamente");
+            }
+            ;
+        }
     }
 }
